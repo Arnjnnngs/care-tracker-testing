24e700462da38474892a740e53bc636951a8cd3d
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-19 00:28:06 -0500

    v46 confirm step before clearing chemo date

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-testing-v45';
+const CACHE = 'caretracker-testing-v46';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
