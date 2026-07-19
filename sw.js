6aac4ca8f1f238601382e02cd374caa5b3d848c3
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-19 01:03:27 -0500

    v47: fix redundant Available text, instant tab-scroll, select/textarea re-render guard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-testing-v46';
+const CACHE = 'caretracker-testing-v47';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
