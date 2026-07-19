54b0acdf0b37c7cc47bef429252410f8faa912ec
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-18 22:12:03 -0500

    Testing UX fixes v40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-testing-v39';
+const CACHE = 'caretracker-testing-v40';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
