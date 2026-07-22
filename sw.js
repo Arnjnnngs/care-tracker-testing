3df53cc866926807ee031f1f01b90e42a0bd545e
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-22 16:58:25 -0500

    v65: force prompt SW updates so already-open tabs pick up fixes (found during UC11 QA)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-testing-v64';
+const CACHE = 'caretracker-testing-v65';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
